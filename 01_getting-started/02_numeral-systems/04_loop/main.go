f78c5922543c80b2d4ea90a8fb0a076aaabef3da
kwonslog <65941166+kwonslog@users.noreply.github.com>
2024-02-06 00:39:37 +0900

    Update main.go

--- 01_getting-started/02_numeral-systems/04_loop/main.go
+++ 01_getting-started/02_numeral-systems/04_loop/main.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 func main() {
+	// 1000000 부터 1000100 까지 10진수, 2진수, 16진수(소문자)를 출력한다.
 	for i := 1000000; i < 1000100; i++ {
 		fmt.Printf("%d \t %b \t %x \n", i, i, i)
 	}
