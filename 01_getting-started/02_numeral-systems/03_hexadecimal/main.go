e22543e8c01d0bda01def53acf1f6288e0ccc876
kwonslog <65941166+kwonslog@users.noreply.github.com>
2024-02-06 00:38:46 +0900

    Update main.go

--- 01_getting-started/02_numeral-systems/03_hexadecimal/main.go
+++ 01_getting-started/02_numeral-systems/03_hexadecimal/main.go
@@ -6,5 +6,7 @@ func main() {
 	//	fmt.Printf("%d - %b - %x \n", 42, 42, 42)
 	//	fmt.Printf("%d - %b - %#x \n", 42, 42, 42)
 	//	fmt.Printf("%d - %b - %#X \n", 42, 42, 42)
+	
+	// 값 42를 10진수, 2진수, 16진수(대문자)로 출력한다.
 	fmt.Printf("%d \t %b \t %#X \n", 42, 42, 42)
 }
