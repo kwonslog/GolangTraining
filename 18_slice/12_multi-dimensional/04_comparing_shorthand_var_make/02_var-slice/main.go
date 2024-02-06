76a6dcf8d04782f23489753eaf3b63c3d8ce7c86
kwonslog <65941166+kwonslog@users.noreply.github.com>
2024-02-06 16:29:30 +0900

    Update main.go

--- 18_slice/12_multi-dimensional/04_comparing_shorthand_var_make/02_var-slice/main.go
+++ 18_slice/12_multi-dimensional/04_comparing_shorthand_var_make/02_var-slice/main.go
@@ -9,6 +9,9 @@ func main() {
 	var students [][]string
 	student[0] = "Todd"
 	// student = append(student, "Todd")
+
+	// 출력결과는 fmt.Println 에서 [] 이렇게 처리한다.
+	// 하지만 실제 값은 nil 이다.
 	fmt.Println(student)
 	fmt.Println(students)
 }
